788f539b1621a659f8168a4245b1ec95fd548cca
tungnv211996 <tungnv21199@gmail.com>
2019-05-22 16:29:11 +0700

    state is immutable

--- src/reducers/tasks.js
+++ src/reducers/tasks.js
@@ -1,7 +1,7 @@
 import * as types from '../contants/ActionTypes';
-var randomID = () => {
-    return '_' + Math.random().toString(36).substr(2, 9);
-};
+// var randomID = () => {
+//     return '_' + Math.random().toString(36).substr(2, 9);
+// };
 
 var taskReducer = (tasks = [], action) => {
     switch (action.type) {
@@ -10,14 +10,14 @@ var taskReducer = (tasks = [], action) => {
             tasks = action.tasks
             return tasks;
         case types.ADD_TASK:
-            tasks.push(action.task)
-            return [...tasks];
+            // tasks.push(action.task)
+            return [...tasks, action.task];
         case types.DELETE_TASK:
             tasks = tasks.filter(i => i.id !== action.id)
             return [...tasks];
         case types.EDIT_TASK:
-            tasks = tasks.map(i => i.id === action.task.id ? action.task : i);
-            return [...tasks];
+            // tasks = tasks.map(i => i.id === action.task.id ? action.task : i);
+            return [...tasks.map(i => i.id === action.task.id ? action.task : i)];
         case types.FILTER:
             let tasksFilter = [...tasks];
             tasksFilter= tasksFilter.filter(i => i.status === action.status)
